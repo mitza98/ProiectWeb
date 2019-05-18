80fc2af3c10e9b0088c2fcdbd8d06a9d0ffd679f
Mihai Cerneanu <mihai.cerneanu@gmail.com>
2019-05-18 16:26:12 +0300

    add hero structure

--- footer.php
+++ footer.php
@@ -1,3 +1,5 @@
-  
+  <footer class="footer">
+      <p class="footer__copy">&copy; Catalog Online Mihai Cerneanu 2019</p>
+  </footer>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php include_once "header.php"; ?>
 
+<section class="hero">
+  <div class="container">
+    <div class="hero__details">
+      <h1 class="hero__intro">Catalog Online</h1>
+      <form action="" class="login__form">
+        <input type="text" class="login__input" placeholder="username">
+        <input type="text" class="login__input"placeholder="password">
+        <button class="login__btn">Log In</button>
+        <a href="signup.php" class="login__btn">Sign Up</a>
+      </form>
+    </div>
+  </div>
+</section>
 
 <?php include_once "footer.php"; ?>
